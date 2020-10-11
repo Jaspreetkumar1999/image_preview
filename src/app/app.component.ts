883c5a9470160e6752cd95c4ed4131ee1cd417cd
Jaspreet Kumar <jaspreetkumar0315@gmail.com>
2020-10-11 16:40:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as $ from 'jquery'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'imgagePreviw';
+
+ public previewFile(input){
+  var file = $("input[type=file]").get(0).files[0];
+
+  if(file){
+      var reader = new FileReader();
+
+      reader.onload = function(){
+          $("#previewImg").attr("src", reader.result);
+      }
+
+      reader.readAsDataURL(file);
+  }
+
+
+  }
 }
